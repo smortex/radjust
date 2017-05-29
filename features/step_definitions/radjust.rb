29603fe9098b3e3b3a8bb59e5eb77217d1d26a0a
Romain Tartière <romain@blogreen.org>
2017-05-29 10:15:08 +0200

    Avoid race condition
    The server's socket must be present for the client to successfully
    start.

--- features/step_definitions/radjust.rb
+++ features/step_definitions/radjust.rb
@@ -1,6 +1,15 @@
 require 'English'
 
 When(/^I synchronize "([^"]*)" \-> "([^"]*)"$/) do |source, destination|
-  `./server/server #{tmp_file_name(destination)} & ./client/client #{tmp_file_name(source)} & wait`
-  expect($CHILD_STATUS.success?).to be_truthy
+  server_pid = spawn("./server/server #{tmp_file_name(destination)}")
+
+  sleep 0.001 until File.exist?('socket')
+
+  client_pid = spawn("./client/client #{tmp_file_name(source)}")
+
+  Process.wait(server_pid)
+  expect($CHILD_STATUS.success?).to be true
+
+  Process.wait(client_pid)
+  expect($CHILD_STATUS.success?).to be true
 end
